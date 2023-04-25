e9b3b747ccab8854ac7217763ad1a3716aa311db
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-04-25 17:57:41 +0900

    スキャフォールディングでコンテキストモジュールを作成

---
+++ lib/nano_planner/schedule/plan_item.ex
@@ -0,0 +1,15 @@
+defmodule NanoPlanner.Schedule.PlanItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "plan_item" do
+    timestamps()
+  end
+
+  @doc false
+  def changeset(plan_item, attrs) do
+    plan_item
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
