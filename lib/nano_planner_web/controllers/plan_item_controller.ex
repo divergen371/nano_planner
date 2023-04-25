f5c82136c54eda51b9298c9699d334526dd1972f
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-04-25 20:51:29 +0900

    plan_item#indexのアクションを実装

--- lib/nano_planner_web/controllers/plan_item_controller.ex
+++ lib/nano_planner_web/controllers/plan_item_controller.ex
@@ -1,7 +1,9 @@
 defmodule NanoPlannerWeb.PlanItemController do
   use NanoPlannerWeb, :controller
+  alias NanoPlanner.Schedule
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    plan_items = Schedule.list_plan_items()
+    render(conn, "index.html", plan_items: plan_items)
   end
 end
