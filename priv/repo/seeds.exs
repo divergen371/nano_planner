9868d8367daf9b9d147797e5d05540fe494e91c7
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-04-27 15:41:07 +0900

    シードスクリプトの書き換え。スキーマ構造体の時刻に関するフィールドのデータ型を訂正

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,17 +1,27 @@
 import NanoPlanner.Repo
 alias NanoPlanner.Schedule.PlanItem
+time0 =
+  "Etc/UTC"
+  |> DateTime.now!()
+  |> DateTime.truncate(:second)
 
 insert!(%PlanItem{
   name: "読書",
-  description: "『走れメロス』を読む"
+  description: "『走れメロス』を読む",
+  start_at: time0,
+  ends_at: time0
 })
 
 insert!(%PlanItem{
   name: "買い物",
-  description: "洗剤を買う"
+  description: "洗剤を買う",
+  start_at: time0,
+  ends_at: time0
 })
 
 insert!(%PlanItem{
   name: "帰省",
-  description: "新幹線の指定席を取る。\nお土産を買う"
+  description: "新幹線の指定席を取る。\nお土産を買う",
+  start_at: time0,
+  ends_at: time0
 })
