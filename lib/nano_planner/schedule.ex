eb4985664eb7ce00ad8ca37903b2c1fc8f99c079
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-01 01:00:50 +0900

    データベースから予定を引っ張ってくるタイミングで時刻をAsia/Tokyoに変換

--- lib/nano_planner/schedule.ex
+++ lib/nano_planner/schedule.ex
@@ -7,5 +7,15 @@ defmodule NanoPlanner.Schedule do
     PlanItem
     |> order_by(asc: :starts_at, asc: :ends_at, asc: :id)
     |> Repo.all()
+    |> convert_datetime()
+  end
+
+  defp convert_datetime(items) do
+    Enum.map(items, fn item ->
+      Map.merge(item, %{
+        starts_at: DateTime.shift_zone!(item.starts_at, "Asia/Tokyo"),
+        ends_at: DateTime.shift_zone!(item.ends_at, "Asia/Tokyo")
+      })
+    end)
   end
 end
