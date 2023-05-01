62d5055c7d304722a4461c231b4761c8264ef76c
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-01 14:35:56 +0900

    秒とタイムゾーンの表示を省略する。 「年」「月」「日」を追加した。 月と日が一桁の場合0埋めしない。

--- lib/nano_planner_web/views/plan_item_view.ex
+++ lib/nano_planner_web/views/plan_item_view.ex
@@ -1,15 +1,17 @@
 defmodule NanoPlannerWeb.PlanItemView do
   use NanoPlannerWeb, :view
 
+  alias Timex.Format.DateTime.Formatters.Strftime
+
   def format_duration(item) do
     Enum.join([format_starts_at(item), "〜", format_ends_at(item)], " ")
   end
 
   defp format_starts_at(item) do
-    item.starts_at
+    Strftime.format!(item.starts_at, "%Y年%-m月%-d日 %H:%M")
   end
 
   defp format_ends_at(item) do
-    item.ends_at
+    Strftime.format!(item.ends_at, "%Y年%-m月%-d日 %H:%M")
   end
 end
