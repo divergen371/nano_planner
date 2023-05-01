48195e0c20c223fcbc58d7dd180b9728bbaf92a0
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-01 14:40:37 +0900

    開始日時が今年であれば年表示を省略するようにした。

--- lib/nano_planner_web/views/plan_item_view.ex
+++ lib/nano_planner_web/views/plan_item_view.ex
@@ -8,7 +8,12 @@ defmodule NanoPlannerWeb.PlanItemView do
   end
 
   defp format_starts_at(item) do
-    Strftime.format!(item.starts_at, "%Y年%-m月%-d日 %H:%M")
+    time_zone = Application.get_env(:nano_planner, :default_time_zone)
+    if item.starts_at.year == DateTime.now!(time_zone).year do
+      Strftime.format!(item.starts_at, "%-m月%-d日 %H:%M")
+    else
+      Strftime.format!(item.starts_at, "%Y年%-m月%-d日 %H:%M")
+    end
   end
 
   defp format_ends_at(item) do
