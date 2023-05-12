e5114c19be29741dfd177e2d03664ffae41d27a0
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-13 01:09:30 +0900

    ちょっと実験

--- priv/v02/ch14/anon2.exs
+++ priv/v02/ch14/anon2.exs
@@ -1,10 +1,13 @@
 double_or_triple = fn
   :a, n -> {:a, n * 2}
   :b, n -> {:b, n * 3}
+  _, n -> {:anything, n}
 end
 
 x = double_or_triple.(:a, 7)
 y = double_or_triple.(:b, 7)
+z = double_or_triple.(:c, 7)
 
 IO.inspect(x)
 IO.inspect(y)
+IO.inspect(z)
