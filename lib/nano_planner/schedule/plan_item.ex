1e6ccd65497eb0cb21215ce0452d67a3bb244d65
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-17 22:13:11 +0900

    👍 日時分割のために仮想フィールドを定義 / 予定に関する日時入力のUI改善
    開始日時と終了日時の日付、時、分の計6つの仮想フィールドを用意。

--- lib/nano_planner/schedule/plan_item.ex
+++ lib/nano_planner/schedule/plan_item.ex
@@ -11,12 +11,29 @@ defmodule NanoPlanner.Schedule.PlanItem do
     field :description, :string, default: ""
     field :starts_at, :utc_datetime
     field :ends_at, :utc_datetime
+    field :s_date, :date, virtual: true
+    field :s_hour, :integer, virtual: true
+    field :s_minute, :integer, virtual: true
+    field :e_date, :date, virtual: true
+    field :e_hour, :integer, virtual: true
+    field :eminute, :integer, virtual: true
     timestamps(type: :utc_datetime_usec)
   end
+
+  @allowed_fields [
+    :name,
+    :description,
+    :s_date,
+    :s_hour,
+    :s_minute,
+    :e_date,
+    :s_hour,
+    :s_minute
+  ]
 
   @doc false
   def changeset(%PlanItem{} = plan_item, attrs) do
     plan_item
-    |> cast(attrs, [:name, :description, :starts_at, :ends_at])
+    |> cast(attrs, @allowed_fields)
   end
 end
