d90ee62914eda7eaabb9adf974f1252e377730d3
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-19 16:00:24 +0900

    :sparkles: データベースに終日フラグを格納するall_dayカラムを追加

--- priv/repo/migrations/20230425084440_create_plan_item.exs
+++ priv/repo/migrations/20230425084440_create_plan_item.exs
@@ -3,10 +3,11 @@ defmodule NanoPlanner.Repo.Migrations.CreatePlanItem do
 
   def change do
     create table(:plan_item) do
-      add :name, :string, null: false
-      add :description, :text, null: false
-      add :starts_at, :utc_datetime, null: false
-      add :ends_at, :utc_datetime, null: false
+      add(:name, :string, null: false)
+      add(:description, :text, null: false)
+      add(:all_day, :boolean, null: false, default: false)
+      add(:starts_at, :utc_datetime, null: false)
+      add(:ends_at, :utc_datetime, null: false)
 
       timestamps()
     end
