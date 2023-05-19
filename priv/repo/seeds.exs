53243baa28c17b7082b5e21cc998ff74007f70ec
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-19 16:47:17 +0900

    :seedling:

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -67,22 +67,22 @@ insert!(%PlanItem{
 
 date0 = DateTime.to_date(time0)
 
-insert!(%PlanItem{
+%PlanItem{
   name: "私の誕生日",
   description: "",
   all_day: true,
-  starts_at: Timex.shift(time0, days: 7),
-  ends_at: Timex.shift(time0, days: 8),
   starts_on: Date.add(date0, 7),
   ends_on: Date.add(date0, 7)
-})
+}
+|> NanoPlanner.Schedule.set_time_boundaries()
+|> insert!()
 
-insert!(%PlanItem{
+%PlanItem{
   name: "社員研修",
   description: "都内某所",
   all_day: true,
-  starts_at: Timex.shift(time0, days: 14),
-  ends_at: Timex.shift(time0, days: 17),
   starts_on: Date.add(date0, 14),
   ends_on: Date.add(date0, 16)
-})
+}
+|> NanoPlanner.Schedule.set_time_boundaries()
+|> insert!()
