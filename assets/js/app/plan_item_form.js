675510771ee9910fe818f9718a4de773852b6690
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-21 17:22:27 +0900

    :+1: 終日チェックボックスの状態を調べる

--- assets/js/app/plan_item_form.js
+++ assets/js/app/plan_item_form.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form.js-plan-item");
 
   if (form) {
-    console.log("loaded!!");
+    const allDay = form.querySelector(
+      "input[name='plan_item[all_day]'][type='checkbox']"
+    );
+
+    console.log(allDay.checked);
   }
 });
