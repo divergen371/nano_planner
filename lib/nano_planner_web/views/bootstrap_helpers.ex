1fb60c0e2a4984b5e42ec91b06d77de09ca8232b
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-23 18:14:07 +0900

    :sparkles: Bootstrapを活用してテキスト入力欄を作っていくためのヘルパー関数

--- lib/nano_planner_web/views/bootstrap_helpers.ex
+++ lib/nano_planner_web/views/bootstrap_helpers.ex
@@ -1,7 +1,24 @@
 defmodule NanoPlannerWeb.BootstrapHelpers do
-  def form_control_class(form, field) do
-    if Keyword.has_key?(form.errors, field),
-      do: "form-control is-invalid",
-      else: "form-control"
+  import Phoenix.HTML.Form
+
+  @moduledoc """
+  Bootstrap style helper
+  """
+  def bootstrap_text_input(form, field, opts \\ []) do
+    class = form_control_class(form, field, opts)
+    opts = Keyword.put(opts, :class, class)
+    text_input(form, field, opts)
   end
+
+  defp form_control_class(form, field, opts) do
+    opts
+    |> Keyword.get(:class, "")
+    |> add_class_token("form-control")
+    |> add_class_token("is-invalid", Keyword.has_key?(form.errors, field))
+  end
+
+  defp add_class_token("" = _class, token), do: token
+  defp add_class_token(class, token), do: class <> " " <> token
+  defp add_class_token(class, token, true), do: class <> " " <> token
+  defp add_class_token(class, _token, false), do: class
 end
