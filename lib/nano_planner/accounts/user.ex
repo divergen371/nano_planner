078935b803ad20092ae3417e556179417edb2edd
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-24 23:50:11 +0900

    :bug: タイポ修正

--- lib/nano_planner/accounts/user.ex
+++ lib/nano_planner/accounts/user.ex
@@ -2,7 +2,7 @@ defmodule NanoPlanner.Accounts.User do
   use Ecto.Schema
 
   schema "users" do
-    field :login_user, :string
+    field :login_name, :string
     field :hashed_password, :string, redact: true
 
     timestamps(type: :utc_datetime_usec)
