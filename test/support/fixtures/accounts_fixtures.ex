722db4fd1b95e101bc4c98ae0f770aaaca8655c8
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-25 13:02:11 +0900

    :recycle: フィクスチャードキュメント追加

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -5,8 +5,14 @@ defmodule NanoPlanner.AccountsFixtures do
   import NanoPlanner.Repo, only: [insert!: 1]
   alias NanoPlanner.Accounts.User
 
+  @doc """
+  テスト用のユーザー名の自動生成
+  """
   def unique_login_name, do: "user#{System.unique_integer()}"
 
+  @doc """
+  ユーザー名とパスワードを束縛したUser構造体を返す
+  """
   def user_fixture(attrs \\ []) do
     login_name = Keyword.get(attrs, :login_name, unique_login_name())
     hashed_password = Bcrypt.hash_pwd_salt(login_name <> "123!")
