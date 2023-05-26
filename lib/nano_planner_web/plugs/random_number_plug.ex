ed67127714a70612c6400c0e184a3ef1fd321912
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-26 15:50:41 +0900

    :alembic: プラグモジュールを定義してみる

---
+++ lib/nano_planner_web/plugs/random_number_plug.ex
@@ -0,0 +1,13 @@
+defmodule NanoPlannerWeb.RandomNumberPlug do
+  import Plug.Conn
+
+  def init(opts) do
+    Keyword.merge([min: 1, max: 100], opts)
+  end
+
+  def call(conn, opts) do
+    min = opts[:min]
+    max = opts[:max]
+    assign(conn, :number, Enum.random(min..max))
+  end
+end
