1a4984120b9fa1474768ad0d1ef82a4e8578393f
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-27 00:40:49 +0900

    :sparkles: :test_tube: ログイン済みのとき、トップページにリダイレクトする

--- lib/nano_planner_web/plugs/user_auth.ex
+++ lib/nano_planner_web/plugs/user_auth.ex
@@ -1,6 +1,8 @@
 defmodule NanoPlannerWeb.UserAuth do
   import Plug.Conn
+  import Phoenix.Controller, only: [redirect: 2]
   alias NanoPlanner.Accounts
+  alias NanoPlannerWeb.Router.Helpers, as: Routes
 
   def fetch_current_user(conn, _opts) do
     session_token = get_session(conn, :session_token)
@@ -8,6 +10,16 @@ defmodule NanoPlannerWeb.UserAuth do
     if session_token do
       user = Accounts.get_user_by_session_token(session_token)
       assign(conn, :current_user, user)
+    else
+      conn
+    end
+  end
+
+  def redirect_if_user_is_authenticated(conn, _opts) do
+    if conn.assigns[:current_user] do
+      conn
+      |> redirect(to: Routes.top_path(conn, :index))
+      |> halt()
     else
       conn
     end
