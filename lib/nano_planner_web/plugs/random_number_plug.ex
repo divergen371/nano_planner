317fe08e31fdc30f36688b56e6745640cb1b50c1
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-26 17:31:15 +0900

    :alembic: ビヘイビア

--- lib/nano_planner_web/plugs/random_number_plug.ex
+++ lib/nano_planner_web/plugs/random_number_plug.ex
@@ -1,10 +1,14 @@
 defmodule NanoPlannerWeb.RandomNumberPlug do
   import Plug.Conn
 
+  @behaviour Plug
+
+  @impl Plug
   def init(opts) do
     Keyword.merge([min: 1, max: 100], opts)
   end
 
+  @impl Plug
   def call(conn, opts) do
     min = opts[:min]
     max = opts[:max]
