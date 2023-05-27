81019450e87ea6c9230628b64e58d2bb4ad72ae4
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-27 16:14:05 +0900

    :sparkles: ユーザー認証機能実装

--- lib/nano_planner_web/controllers/user_session_controller.ex
+++ lib/nano_planner_web/controllers/user_session_controller.ex
@@ -1,13 +1,28 @@
 defmodule NanoPlannerWeb.UserSessionController do
+  alias NanoPlanner.Accounts
   use NanoPlannerWeb, :controller
-
-
 
   def new(conn, _params) do
     render(conn, "new.html")
   end
 
-  def create(conn, _params) do
-    redirect(conn, to: Routes.top_path(conn, :index))
+  def create(conn, %{"user" => user_params}) do
+    user =
+      Accounts.get_user_by_login_name_and_password(
+        user_params["login_name"],
+        user_params["password"]
+      )
+
+    if user do
+      session_token = Accounts.generate_session_token(user)
+
+      conn
+      |> configure_session(renew: true)
+      |> clear_session()
+      |> put_session(:session_token, session_token)
+      |> redirect(to: Routes.top_path(conn, :index))
+    else
+      render(conn, "new.html")
+    end
   end
 end
