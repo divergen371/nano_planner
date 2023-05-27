62b2fe1984391fa9107650e65fb35d9d721e4ed6
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-27 15:08:38 +0900

    :test_tube: ユーザー認証の仮実装

--- lib/nano_planner_web/controllers/user_session_controller.ex
+++ lib/nano_planner_web/controllers/user_session_controller.ex
@@ -6,4 +6,8 @@ defmodule NanoPlannerWeb.UserSessionController do
   def new(conn, _params) do
     render(conn, "new.html")
   end
+
+  def create(conn, _params) do
+    redirect(conn, to: Routes.top_path(conn, :index))
+  end
 end
