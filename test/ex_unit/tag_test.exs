d27816370a84a6da1d0f2db3330505307e15448b
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-05-27 21:44:59 +0900

    :test_tube: タグの便利な使い方。test/supportディレクトリにあるファイル内のsetupも参照できる

--- test/ex_unit/tag_test.exs
+++ test/ex_unit/tag_test.exs
@@ -1,18 +1,13 @@
 defmodule NanoPlanner.TagTest do
   use NanoPlanner.DataCase
-  import NanoPlanner.AccountsFixtures
 
-  setup context do
-    login_name = context[:login_name] || "alice"
-    user = user_fixture(login_name: login_name)
-    {:ok, user: user}
-  end
-
+  @tag :login
   @tag login_name: "bob"
   test "ログイン名はbobである", %{user: user} do
     assert user.login_name == "bob"
   end
 
+  @tag :login
   test "ログイン名はaliceである", %{user: user} do
     assert user.login_name == "alice"
   end
